f2dca3737e757ee06ad26ac771e335449ae5faa3
DarrenShan04 <39553146+DarrenShan04@users.noreply.github.com>
2018-05-23 15:28:51 +0530

    Add files via upload

---
+++ Calculation.py
@@ -0,0 +1,9 @@
+def calculation(a, b):
+    print( a + b )
+    print( a - b )
+    print( a * b )
+    if a != 0 and b != 0:
+        print(a / b)
+    else:
+        print('division with 0 is not possible')
+        
