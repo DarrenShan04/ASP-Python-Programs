1d377a72976ea2054196ec60c757876bd53b23da
DarrenShan04 <39553146+DarrenShan04@users.noreply.github.com>
2018-05-30 13:41:13 +0530

    S

--- InstrumentGlove/PianoAudioFiles/pianotest.py
+++ InstrumentGlove/PianoAudioFiles/pianotest.py
@@ -38,9 +38,10 @@ for key in colorValues:
 
         
         if sensedcolor[0] in colorValues[key][0] and sensedcolor[1] in colorValues[key][1] and sensedcolor[2] in colorValues[key][2] and sensedcolor[3] in colorValues[key][3] and lux in colorValues[key][4]:
+                print('\nc')
+                print(colorValues[key][0]
                 print(sensedcolor)
                 print(lux)
-                print('\nc')
                 print(key)
                 #print(pianoTunes[key])
             #os.system(guitarTunes[key])
