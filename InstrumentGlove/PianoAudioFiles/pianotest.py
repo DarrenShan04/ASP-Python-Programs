257e2742cf48387847443081c910426969f7e82d
DarrenShan04 <39553146+DarrenShan04@users.noreply.github.com>
2018-05-30 13:43:50 +0530

    S

--- InstrumentGlove/PianoAudioFiles/pianotest.py
+++ InstrumentGlove/PianoAudioFiles/pianotest.py
@@ -39,7 +39,7 @@ for key in colorValues:
         
         if sensedcolor[0] in colorValues[key][0] and sensedcolor[1] in colorValues[key][1] and sensedcolor[2] in colorValues[key][2] and sensedcolor[3] in colorValues[key][3] and lux in colorValues[key][4]:
                 print('\nc')
-                print(colorValues[key][0]
+                print(colorValues[key][0])
                 print(sensedcolor)
                 print(lux)
                 print(key)
