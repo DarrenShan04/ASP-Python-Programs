1180ed17eeefecb499aa052257c8c8375d469c49
DarrenShan04 <39553146+DarrenShan04@users.noreply.github.com>
2018-05-31 10:14:11 +0530

    S

--- InstrumentGlove/PianoAudioFiles/pianotest2.py
+++ InstrumentGlove/PianoAudioFiles/pianotest2.py
@@ -55,9 +55,10 @@ ravg, gavg, bavg, cavg, lavg = run5times()
 
 pianoTunes = {'green' : 'aplay PianoC.wav', 'brown' : 'aplay PianoC#.wav', 'blue' : 'aplay PianoD.wav', 'black' : 'PianoD#', 'orange' : 'aplay PianoE.wav', 'yellow' : 'aplay PianoF.wav', 'darkgreen' : 'aplay PianoF#.wav', 'magenta':'aplay PianoG.wav', 'dark purple' : 'aplay PianoG#.wav', 'pink':'aplay PianoA.wav', 'darkblue': 'aplay PianoA#.wav', 'red' : 'PianoB.wav'}
 
-run5times()
 
 while True:
+    tcs.get_raw_data()
+    ravg, gavg, bavg, cavg, lavg = run5times()
     for key in colorValues:
         if (ravg in colorValues[key][0]) and (gavg in colorValues[key][1]) and (bavg in colorValues[key][2]) and (cavg in colorValues[key][3]) and (lavg in colorValues[key][4]):
                 os.system(pianoTunes[key])
