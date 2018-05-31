493832f8ec0c14d45260315b618ad79dbbeb1e96
DarrenShan04 <39553146+DarrenShan04@users.noreply.github.com>
2018-05-31 15:34:08 +0530

    removed lux
    removed lux

--- InstrumentGlove/FinalCode/PianoCode.py
+++ InstrumentGlove/FinalCode/PianoCode.py
@@ -68,7 +68,7 @@ while True:
     print(averagevalues)    
     ravg, gavg, bavg, cavg, lavg = averagevalues
     for key in colorValues:
-        if (ravg in colorValues[key][0]) and (gavg in colorValues[key][1]) and (bavg in colorValues[key][2]) and (cavg in colorValues[key][3]) and (lavg in colorValues[key][4]):
+        if (ravg in colorValues[key][0]) and (gavg in colorValues[key][1]) and (bavg in colorValues[key][2]) and (cavg in colorValues[key][3]):
             os.system(pianoTunes[key])
             time.sleep(0.05)
         continue
