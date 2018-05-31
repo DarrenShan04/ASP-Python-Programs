07f1b31c535f3883f387121a62dc988b2568ac62
DarrenShan04 <39553146+DarrenShan04@users.noreply.github.com>
2018-05-31 12:58:29 +0530

    S

--- InstrumentGlove/FinalCode/fincode.py
+++ InstrumentGlove/FinalCode/fincode.py
@@ -73,6 +73,7 @@ while True:
         if (ravg in colorValues[key][0]) and (gavg in colorValues[key][1]) and (bavg in colorValues[key][2]) and (cavg in colorValues[key][3]) and (lavg in colorValues[key][4]):
             if key.startswith('BG'):
                 instrument = colorValues[key][5]
+                continue
             else:
                 pass
     if instrument == 'piano':
