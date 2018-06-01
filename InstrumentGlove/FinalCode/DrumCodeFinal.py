a32757320e56e65bd2d2029289eb9b3c95540687
DarrenShan04 <39553146+DarrenShan04@users.noreply.github.com>
2018-06-01 09:46:07 +0530

    S

--- InstrumentGlove/FinalCode/DrumCodeFinal.py
+++ InstrumentGlove/FinalCode/DrumCodeFinal.py
@@ -57,10 +57,13 @@ def run5times():
 drumTunes = {'magenta' : 'DrumBass.wav', 'blue' : 'DrumTomLow.wav', 'pink' : 'DrumTomHi.wav', 'red' : 'DrumSnare.wav', 'orange' : 'DrumHiHat.wav', 'green' : 'DrumFloorTom.wav', 'yellow' : 'DrumCrashCymbal.wav'}
 
 while True:
+    print('a')
     averagevalues=run5times()  
     ravg, gavg, bavg, cavg, lavg = averagevalues
     for key in colorValues:
+        print('b')
         if (ravg in colorValues[key][0]) and (gavg in colorValues[key][1]) and (bavg in colorValues[key][2]) and (cavg in colorValues[key][3]):
+            print('c')
             print(drumTunes[key])
             os.system(drumTunes[key])
             time.sleep(0.05)
